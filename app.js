a9e362eb87f1f4fa9b83a39e010d58dee22957b3
scrivy <scrivinator@gmail.com>
2014-04-17 04:31:36 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var exec = require('child_process').exec
+  , fs = require('fs')
+  , express = require('express');
+
+var takepic = function() {
+  var child = exec('fswebcam -d /dev/video0 -r 640x480 --rotate -90 --no-banner --jpeg 25 -q '+Date.now()+'.jpg', { cwd: __dirname + '/pics' }, function(err,stdout,stderr) {
+    if (err) console.log(err);
+  });
+};
+
+setInterval(takepic, 2000);
+
+// watch for changes
+var latestfilename;
+fs.watch(__dirname + '/pics', function(event, filename) {
+  latestfilename = filename;
+});
+
+// webserver
+var app = express();
+
+app.get('/latestpic', function(req, res) {
+  res.send(latestfilename);
+});
+
+app.use(express.static(__dirname + '/pics'));
+
+app.listen(8080);
