1dc42171da3db7dbae4ef47cdfe23c70bff7cdb6
scrivy <scrivinator@gmail.com>
2014-04-28 15:43:53 -0700

    rewrote again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var spawn = require('child_process').spawn
+var exec = require('child_process').exec
   , fs = require('fs')
   , http = require('http')
   , Primus = require('primus')
@@ -17,40 +17,10 @@ var requesthandler = function(req, res) {
 var server = http.createServer(requesthandler).listen(5000)
   , primus = new Primus(server, { transformer: 'engine.io' });
 
-var findendofjpg = function(buffer) {
-  var i;
-  for (i=0; i<buffer.length; i++) {
-    if (buffer[i] === 255)
-      if (buffer[i+1] === 217)
-        return i+1;
-  };
-  return 0;
+var takepic = function() {
+  var child = exec('fswebcam -d /dev/video0 -r 640x480 --rotate -90 --jpeg 35 -q --no-banner -', {encoding: 'base64'}, function(err, stdout, stderr) {
+    primus.write(stdout);
+  });
 };
 
-var picdata = [];
-var child = spawn('fswebcam', ['-d', '/dev/video0', '-r', '640x480', '--rotate', '-90', '--jpeg', '35', '-q', '--no-banner', '-l', '1', '-']);
-
-child.stdout.on('data', function(data) {
-  var end
-    , pic;
-  if (end = findendofjpg(data)) {
-    picdata.push(data.slice(0,end));
-    pic = Buffer.concat(picdata);
-    primus.write(pic.toString('base64'));
-    fs.writeFile(__dirname + '/pics/' + Date.now() + '.jpg', pic, function(err) {
-      if (err) console.log('write error - ' + err);
-    });
-    picdata = [ data.slice(end+1) ];
-  } else {
-    picdata.push(data);
-  }
-});
-
-child.stderr.on('data', function(data) {
-  console.error(data.toString());
-});
-
-child.on('close', function(code) {
-  console.log('child process exited with code ' + code);
-});
-
+setInterval(function() { takepic(); }, 500);
