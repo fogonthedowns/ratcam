f3d13068154a5357e81f4e099738cb760212882c
scrivy <scrivinator@gmail.com>
2014-04-30 00:26:32 -0700

    made sure another child process isnt started until the previous one is finished

--- app.js
+++ app.js
@@ -17,19 +17,25 @@ var requesthandler = function(req, res) {
 var server = http.createServer(requesthandler).listen(5000,function() { console.log('listening'); })
   , primus = new Primus(server, { transformer: 'engine.io' });
 
-var takepic = function() {
-  var child = exec('fswebcam -d /dev/video0 -r 640x480 --rotate -90 --jpeg 35 -q -', {encoding: 'base64'}, function(err, stdout, stderr) {
-    if (!err && !stderr) {
-      primus.write(stdout);
-      fs.writeFile(__dirname + '/pics/' + Date.now() + '.jpg', new Buffer(stdout, 'base64'), function(err) {
-        if (err) console.error(err);
-      });
-      return 0;
-    }
+var child
+  , ready = true
+  , takepic = function() {
+  if (ready) {
+    ready = false;
+    child = exec('fswebcam -d /dev/video0 -r 640x480 --rotate -90 --jpeg 35 -q -', {encoding: 'base64'}, function(err, stdout, stderr) {
+      ready = true;
+      if (!err && !stderr) {
+        primus.write(stdout);
+        fs.writeFile(__dirname + '/pics/' + Date.now() + '.jpg', new Buffer(stdout, 'base64'), function(err) {
+          if (err) console.error(err);
+        });
+        return 0;
+      }
 
-    if (err) console.log(err);
-    if (stderr) console.error(new Buffer(stderr, 'base64').toString());
-  });
+      if (err) console.log(err);
+      if (stderr) console.error(new Buffer(stderr, 'base64').toString());
+    });
+  }
 };
 
 setInterval(function() { takepic(); }, 2000);
